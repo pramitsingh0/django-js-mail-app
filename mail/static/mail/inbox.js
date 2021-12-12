a28c49101fe0317fd935bab0e1f28f593512f4b9
pramitsingh0 <pramitsingh0@gmail.com>
2021-12-12 17:20:04 +0530

    Completed view mail function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -31,6 +31,25 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    emails.forEach(element => {
+      if (element["archived"]) {
+        return;
+      }
+      const mail = document.createElement('div')
+      mail.setAttribute("id", "emails-lsit")
+      mail.setAttribute("class", "card card-body")
+      mail.style.marginBottom = "10px"
+      mail.innerHTML = element["subject"].bold() + element["sender"] + " | " + element['timestamp']
+      document.querySelector('#emails-view').append(mail)
+      if (element["read"]) {
+        mail.style.backgroundColor = "#e9e9e9";
+      }
+    });
+  })
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }

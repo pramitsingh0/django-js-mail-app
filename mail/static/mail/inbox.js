4fa00fb47c19c2bc496fa34e10aa951c79eec88d
pramitsingh0 <pramitsingh0@gmail.com>
2021-12-13 18:01:25 +0530

    Completed the view mail function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -30,6 +30,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
@@ -38,17 +39,20 @@ function load_mailbox(mailbox) {
       if (element["archived"]) {
         return;
       }
-      const mail = document.createElement('div')
-      mail.setAttribute("id", "emails-lsit")
-      mail.setAttribute("class", "card card-body")
-      mail.style.marginBottom = "10px"
-      mail.innerHTML = element["subject"].bold() + element["sender"] + " | " + element['timestamp']
-      document.querySelector('#emails-view').append(mail)
+      const mail = document.createElement('div');
+      mail.setAttribute("class", "card card-body");
+      mail.style.marginBottom = "10px";
+      mail.innerHTML = element["subject"].bold() + element["sender"] + " | " + element['timestamp'];
+      mail.addEventListener('click', () => view_mail(element["id"]));
       if (element["read"]) {
         mail.style.backgroundColor = "#e9e9e9";
       }
+      document.querySelector('#emails-view').append(mail);
     });
   })
+
+
+  
   
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -81,4 +85,41 @@ function send_mail(event) {
       }
     })
   return false;
+}
+
+function view_mail(mailId) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#email-view').innerHTML = "";
+
+  fetch('/emails/' + mailId)
+  .then(response => response.json())
+  .then(mail => {
+    const mail_div = document.createElement('div')
+    mail_div.setAttribute('id', 'mail-container')
+    mail_div.innerHTML = `
+                          <div class="card" style="width: 18rem;">
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item"><b>Sender:</b> ${mail["sender"]}</li>
+                              <li class="list-group-item"><b>Recipients:</b> ${mail["recipients"]}</li>
+                              <li class="list-group-item"><b>Subject:</b> ${mail["subject"]}</li>
+                              <li class="list-group-item">${mail["timestamp"]}</li>
+                            </ul>
+                          </div>
+
+                          
+                          <div id="mail-body" class="card card-body">
+                            ${mail["body"]}
+                          </div>
+                          `;
+    document.querySelector('#email-view').append(mail_div);
+    document.querySelector('#mail-body').style.marginTop = "40px";
+    if (!mail['read']) {
+      fetch('/emails/' + mailId, {
+        method: 'PUT',
+        body: JSON.stringify({ read : true })
+      })
+    }
+  })
 }
